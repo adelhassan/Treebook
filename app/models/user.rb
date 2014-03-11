ad468805bf9cb9eb9f8fbfd42060e969ddbd8a69
Adel Hassan <Ahassan@ITManager-HP.cpmadina.local>
2014-03-11 13:13:41 +0300

    added boostarb
    write
    
    w
    --
    
    --insert--
    q

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,16 @@ class User < ActiveRecord::Base
 
 	end
 
+
+    validates :first_name, presence:true
+    validates :last_name, presence:true
+    validates :profile_name, presence: true, uniqueness: true,
+                  format: {
+                  	with: /a-zA-Z0-9_-/,
+                  	message:'Must be formatted correctly.'
+                  }
 	has_many :statuses
-	
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+test " User should enter his first name " do
+
+
+user=User.new
+
+assert !user.save
+assert !user.errors[:first_name].empty?
+
+end
+test " User should enter his last name " do
+
+
+user=User.new
+
+assert !user.save
+assert !user.errors[:last_name].empty?
+end
+test " User should enter his profile name " do
+
+
+user=User.new
+
+assert !user.save
+assert !user.errors[:profile_name].empty?
+
+end
+
+test "profile name should be unique" do
+
+	
+user=User.new
+    user.profile_name= users(:adel).profile_name
+	assert !user.save
+	assert !user.errors[:profile_name].empty?
+
+	end
+test "profile name should not have space" do
+
+	
+user=User.new
+    user.profile_name= "My profile name has space"
+	assert !user.save
+	assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("Must be formatted correctly")
+
+
+end
 end
