19f90767a8b765b58c83a75da8c8c95cf89992ec
Adel Hassan <Ahassan@ITManager-HP.cpmadina.local>
2014-03-13 16:19:46 +0300

    added boostarb
    write
    
    w
    --
    
    --insert--
    q

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,7 +1,14 @@
 class Status < ActiveRecord::Base
 
 
-belongs_to :user	
+belongs_to :user
+
+validates :content, presence: true,
+            :length {minimum: 2}	
+
+
+validtaes :user_id, presence: true
+
 
 	
 
